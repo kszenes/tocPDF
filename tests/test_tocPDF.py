fb51c26879c09da44adffa30fb120491f1377e2f
Kalman Szenes <szenes.kalman@gmail.com>
2024-09-22 14:33:29 +0200

    fix: test

--- tests/test_tocPDF.py
+++ tests/test_tocPDF.py
@@ -11,8 +11,6 @@ HERE = os.path.abspath(os.path.dirname(__file__))
 class TestTOCExtractor:
     @classmethod
     def setup_class(cls):
-        print(f"{os.listdir(HERE) = }")
-        print(f"{os.listdir(os.path.join(HERE, 'pdfs')) = }")
         cls.fpath = os.path.join(HERE, "pdfs/dotted.pdf")
         cls.outpath = generate_toc_pdf(cls.fpath, 7, 8)
 
